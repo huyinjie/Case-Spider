64c09d543e8732c946725368a760a71e1911423a
huyinjie <huyinjie1998@gmail.com>
2018-05-16 17:16:31 +0800

    update .travis.yml

--- Jonsbo/main.py
+++ Jonsbo/main.py
@@ -13,5 +13,6 @@ Product_Sort_Dict = getProductList.getNameUrlPair(Product_Sort_Dict)
 
 Product_List = getProductList.getNameUrlColorPair(Product_Sort_Dict, Product_List)
 Parameter_List = getProductData.main(Product_List, Parameter_List)
+print("Successfully!")
 updateDatabase.main(Parameter_List)
 
