1b36d2580051e7a545521c084ed21ed56cbf54d4
huyinjie <huyinjie1998@gmail.com>
2018-05-17 16:55:41 +0800

    update CI test

--- Phanteks/main.py
+++ Phanteks/main.py
@@ -1,11 +1,9 @@
-from Phanteks.getProductList import getUrlNamePair
-from Phanteks.getProductList import getUrlList
-
+import getProductList
 
 Product_List = []
 Product_dict = dict()
 
-getUrlList(Product_List)
-getUrlNamePair(Product_List, Product_dict)
+getProductList.getUrlList(Product_List)
+getProductList.getUrlNamePair(Product_List, Product_dict)
 print(Product_dict)
 
