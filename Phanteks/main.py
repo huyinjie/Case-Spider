9076f51ef339fad79070eba6ad6fddd5633d7437
huyinjie <huyinjie1998@gmail.com>
2018-05-17 16:18:18 +0800

    init main.py

---
+++ Phanteks/main.py
@@ -0,0 +1,10 @@
+import getProductList
+
+
+Product_List = []
+Product_dict = dict()
+
+getProductList.getUrlList(Product_List)
+getProductList.getUrlNamePair(Product_List, Product_dict)
+print(Product_dict)
+
