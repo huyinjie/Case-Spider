9c9e682c8e845c69c04e5831a12ee185b1447275
huyinjie <huyinjie1998@gmail.com>
2018-06-01 22:40:09 +0800

    修改了getNameUrlColorPair，增加测试

--- Jonsbo/getProductList.py
+++ Jonsbo/getProductList.py
@@ -35,7 +35,8 @@ def getSortName(string):
 
 
 # My main code
-# 获取key位系列名，v位系列url的dict
+# 传入一个空dict
+# 返回该dict，key为型号，value为url
 def getNameUrlPair(tempdict):
     url = "http://www.jonsbo.com/index.html"
     r = requests.get(url)
@@ -50,7 +51,42 @@ def getNameUrlPair(tempdict):
     return tempdict
 
 
+# 传入一个getNameUrlPair函数返回的dict和一个空list
+# 返回该list，该list为二维数组，其中每个型号的型号名，url，拥有的颜色分别为list[i][0~2]
+
+# def getNameUrlColorPair(tempdict, templist):
+#     index = 0
+#     for k, v in tempdict.items():
+#         singleList = [[], [], []]
+#         r = requests.get(v)
+#         r.encoding = 'utf-8'
+#         soup = BeautifulSoup(r.text, "lxml")
+#
+#         firstlink = soup.find("td", {"id": "pic_txt"})
+#         firstlink = firstlink.a.get('href')
+#         singleList[1] = addPrefix(firstlink)
+#         singleList[0] = getSortName(k)
+#         for item in soup.find_all("td", {"id": "pic_txt"}):
+#             for link in item.find_all('a', href=re.compile("^products_\d")):
+#                 singleList[2].append(link.get_text())
+#         singleList[2] = colorClean((singleList[2]))
+#         templist[index] = singleList
+#         index = index + 1
+#     return templist
+
+
 def getNameUrlColorPair(tempdict, templist):
+    url = "http://www.jonsbo.com/index.html"
+    r = requests.get(url)
+    r.encoding = 'utf-8'
+    soup = BeautifulSoup(r.text, "lxml")
+
+    soup = soup.find("ul", {"id": "subMenu26"})
+    soup = soup.find_all("li")
+    for li in soup:
+        k = li.a.get_text()
+        v = addPrefix(li.a.get('href'))
+        tempdict[k] = v
     index = 0
     for k, v in tempdict.items():
         singleList = [[], [], []]
@@ -69,4 +105,13 @@ def getNameUrlColorPair(tempdict, templist):
         templist[index] = singleList
         index = index + 1
     return templist
+
+
+if __name__ == '__main__':
+    product_url_dict = dict()
+    product_url_dict = getNameUrlPair(product_url_dict)
+    product_url_color_list = [None] * len(product_url_dict.items())
+    product_url_color_list = getNameUrlColorPair(product_url_dict, product_url_color_list)
+    print(product_url_color_list)
+
 
